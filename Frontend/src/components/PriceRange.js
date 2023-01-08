889c7bbc8a9a4dac06861525c389b49bfdc8a128
Abdallah-Elbarkokey <abdallah.elbarkokey@ejust.edu.eg>
2023-01-09 00:12:14 +0200

    connected some component to the backend(add to cart button/ cart page); add cookie for token and userID; modified search bar-> added it to the home page

--- Frontend/src/components/PriceRange.js
+++ Frontend/src/components/PriceRange.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from "react";
 import ReactDom from 'react-dom';
-import "./PriceRange.css"
+import "../style/PriceRange.css"
 
 
 class DoubleRangeSlider extends React.Component {
@@ -11,40 +11,40 @@ class DoubleRangeSlider extends React.Component {
     min: 0,
     max: 200,
     minValueBetween: 10,
-    
+
     currentMin: 55,
     inputMin: 55,
-    
+
     currentMax: 100,
     inputMax: 100
   };
 
- componentDidMount() {
-   const { currentMin, currentMax, max } = this.state;
-   
-   this.minValue.style.width = (currentMin*100)/max + "%";
-   this.maxValue.style.width = (currentMax*100)/max + "%";
-   
-   this.setState({
-     sliderWidth: this.slider.offsetWidth,
-     offsetSliderWidht: this.slider.offsetLeft,
-   })
- }
+  componentDidMount() {
+    const { currentMin, currentMax, max } = this.state;
+
+    this.minValue.style.width = (currentMin * 100) / max + "%";
+    this.maxValue.style.width = (currentMax * 100) / max + "%";
+
+    this.setState({
+      sliderWidth: this.slider.offsetWidth,
+      offsetSliderWidht: this.slider.offsetLeft,
+    })
+  }
 
   setMin = (e) => {
     const { min, max, currentMax, minValueBetween } = this.state;
     const inputMin = e.target.value;
-    
+
     this.setState({
-        inputMin
+      inputMin
     });
-    
-    if((inputMin >= min) && (inputMin <= (currentMax-minValueBetween))){
+
+    if ((inputMin >= min) && (inputMin <= (currentMax - minValueBetween))) {
       this.setState({
         currentMin: parseInt(inputMin)
-      }); 
+      });
 
-      this.minValue.style.width = (inputMin*100)/max + "%";
+      this.minValue.style.width = (inputMin * 100) / max + "%";
     }
   }
 
@@ -53,25 +53,25 @@ class DoubleRangeSlider extends React.Component {
 
     document.addEventListener('mousemove', this.onMouseMoveMin);
     document.addEventListener('mouseup', this.onMouseUpMin);
-    
+
     document.addEventListener('touchmove', this.onMouseMoveMin);
     document.addEventListener('touchend', this.onMouseUpMin);
   }
 
   onMouseMoveMin = (e) => {
     const { min, max, currentMax, minValueBetween, sliderWidth, offsetSliderWidht } = this.state;
-    
+
     const dragedWidht = e.clientX - offsetSliderWidht;
-    const dragedWidhtInPercent = (dragedWidht*100)/sliderWidth;
-    const currentMin = Math.abs(parseInt((max * dragedWidhtInPercent)/100));
-    
+    const dragedWidhtInPercent = (dragedWidht * 100) / sliderWidth;
+    const currentMin = Math.abs(parseInt((max * dragedWidhtInPercent) / 100));
+
     console.log(e.pageX, e.clientX, offsetSliderWidht);
-    
-    console.log(currentMin , (currentMax-minValueBetween));
-    
-    console.log((max * dragedWidhtInPercent)/100);
- 
-    if( (currentMin >= min) && (currentMin <= (currentMax-minValueBetween))){
+
+    console.log(currentMin, (currentMax - minValueBetween));
+
+    console.log((max * dragedWidhtInPercent) / 100);
+
+    if ((currentMin >= min) && (currentMin <= (currentMax - minValueBetween))) {
       this.minValue.style.width = dragedWidhtInPercent + "%";
       this.minValue.dataset.content = currentMin;
 
@@ -85,50 +85,50 @@ class DoubleRangeSlider extends React.Component {
   onMouseUpMin = () => {
     document.removeEventListener('mouseup', this.onMouseUpMin);
     document.removeEventListener('mousemove', this.onMouseMoveMin);
-    
+
     document.removeEventListener('touchend', this.onMouseMoveMin);
     document.removeEventListener('touchmove', this.onMouseUpMin);
   }
-  
-  
+
+
   setMax = (e) => {
     const { min, max, currentMin, currentMax, minValueBetween } = this.state;
 
     const inputMax = e.target.value;
-    
+
     this.setState({
-        inputMax
+      inputMax
     });
 
-    if((inputMax >= currentMin + minValueBetween) && (inputMax <= max)){
-      
+    if ((inputMax >= currentMin + minValueBetween) && (inputMax <= max)) {
+
       this.setState({
         currentMax: parseInt(inputMax)
       });
-      this.maxValue.style.width = (inputMax*100)/max + "%";
+      this.maxValue.style.width = (inputMax * 100) / max + "%";
     }
-      
+
   }
-  
+
   changeMaxValue = (e) => {
     e.preventDefault();
 
     document.addEventListener('mousemove', this.onMouseMoveMax);
     document.addEventListener('mouseup', this.onMouseUpMax);
-    
+
     document.addEventListener('touchmove', this.onMouseMoveMax);
     document.addEventListener('touchend', this.onMouseUpMax);
   }
 
   onMouseMoveMax = (e) => {
-    const { max, currentMin, minValueBetween, sliderWidth, offsetSliderWidht} = this.state; 
+    const { max, currentMin, minValueBetween, sliderWidth, offsetSliderWidht } = this.state;
     const maxWalueThumb = this.maxValue;
     const dragedWidht = e.clientX - offsetSliderWidht;
-    const dragedWidhtInPercent = (dragedWidht*100)/sliderWidth;
-    const currentMax = Math.abs(parseInt((max * dragedWidhtInPercent)/100));
-    
-    if( (currentMax >= (currentMin + minValueBetween)) && (currentMax <= max)){
-      
+    const dragedWidhtInPercent = (dragedWidht * 100) / sliderWidth;
+    const currentMax = Math.abs(parseInt((max * dragedWidhtInPercent) / 100));
+
+    if ((currentMax >= (currentMin + minValueBetween)) && (currentMax <= max)) {
+
       maxWalueThumb.style.width = dragedWidhtInPercent + "%";
       maxWalueThumb.dataset.content = currentMax;
       this.setState({
@@ -141,59 +141,59 @@ class DoubleRangeSlider extends React.Component {
   onMouseUpMax = () => {
     document.removeEventListener('mouseup', this.onMouseUp);
     document.removeEventListener('mousemove', this.onMouseMoveMax);
-    
+
     document.removeEventListener('touchend', this.onMouseUp);
     document.removeEventListener('touchmove', this.onMouseMoveMax);
   }
-  
+
   maxForMin = () => {
-    const { currentMax, minValueBetween} = this.state; 
+    const { currentMax, minValueBetween } = this.state;
     return currentMax - minValueBetween;
   }
-  
+
   minForMax = () => {
-    const { currentMin, minValueBetween} = this.state; 
+    const { currentMin, minValueBetween } = this.state;
     return currentMin + minValueBetween;
   }
 
 
   render() {
     const { min, max, currentMin, inputMin, currentMax, inputMax, minValueBetween } = this.state;
-    
+
     return (
-      <div className="card"> 
+      <div className="card">
         <h2>Double range slider</h2>
         <div className="current-value">
           <label htmlFor="min-input">Min: </label>
-          <input 
-            id="min-input" 
-            type="number" 
-            onChange={this.setMin} 
+          <input
+            id="min-input"
+            type="number"
+            onChange={this.setMin}
             value={inputMin}
             min={min}
-            max={this.maxForMin}/>
-          
-          <br/>
+            max={this.maxForMin} />
+
+          <br />
           <label htmlFor="max-input">Max: </label>
-          <input 
-            id="max-input" 
-            type="number" 
-            onChange={this.setMax} 
+          <input
+            id="max-input"
+            type="number"
+            onChange={this.setMax}
             value={inputMax}
             min={this.minForMax}
-            max={max}/>
-          
+            max={max} />
+
         </div>
-        
+
         <div className="values">
-          <div>{ min }</div>
-          <div>{ max }</div>
+          <div>{min}</div>
+          <div>{max}</div>
         </div>
-          
+
         <div ref={ref => this.slider = ref} id="slider">
 
           <div ref={ref => this.minValue = ref} id="min" data-content={currentMin}>
-            <div ref={ref => this.minValueDrag = ref} id="min-drag" onMouseDown ={this.changeMinValue} onTouchStart={this.changeMinValue}></div>
+            <div ref={ref => this.minValueDrag = ref} id="min-drag" onMouseDown={this.changeMinValue} onTouchStart={this.changeMinValue}></div>
           </div>
 
           <div ref={ref => this.maxValue = ref} id="max" data-content={currentMax}>
