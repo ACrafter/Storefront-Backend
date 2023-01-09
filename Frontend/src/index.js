42792db288bee0d527e9528e1c43808563683f78
ACrafter <ahmed2002yasser.ay@gmail.com>
2023-01-09 17:30:54 +0200

    Readded About Component

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -8,7 +8,7 @@ import Productlist from "./components/home"
 import Navbaar from './components/navbar';
 import Error from './components/error';
 import Login from './components/login';
-// import About from './components/about';
+import About from './components/about';
 import Singup from './components/singup';
 import Producttoadd from './components/addproduct';
 import Cart from './components/cart';
@@ -28,7 +28,7 @@ const HeaderRoute = () => {
 
 				<Route exact path='/' element={<Productlist />} />
 
-				{/* <Route exact path='/about' element={<About />} /> */}
+				<Route exact path='/about' element={<About />} />
 				<Route exact path='/singup' element={<Singup />} />
 				<Route exact path='/cart' element={<Cart />} />
 				<Route exact path='/add' element={<Producttoadd />} />
